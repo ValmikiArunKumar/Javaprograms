d3e40862c6cf658e6df79d46e68fffcbb425b89f
ValmikiArunKumar <152388787+ValmikiArunKumar@users.noreply.github.com>
2023-12-12 14:56:50 +0530

    Add files via upload

--- SumAverage.java
+++ SumAverage.java
@@ -19,7 +19,9 @@ public class SumAverage
 		int n5 = reader.nextInt();
 		int sum = n1 + n2 + n3 + n4 + n5;
 		float avg = (n1 + n2 + n3 + n4 + n5)/5;
-		System.out.println("Sum of " + n1 + ", " + n2 + ", " + n3 + ", " + n4 + ", " + n5 + " = " + sum);
-		System.out.println("Average of " + n1 + ", " + n2 + ", " + n3 + ", " + n4 + ", " + n5 + " = " + avg);
-	}
+                System.out.println(" Sum of " + n1 + " ," + n2 + " , " + n3 + " , " + n4 + " , " + n5 + " =" + sum);	
+                System.out.println(" Average of " + n1 + " ," + n2 + " , " + n3 + " , " + n4 + " , " + n5 + " = " + avg);	
+        }
+                
+
 }
